c0f907566e75cb558df20ee8702003bb66ae3c30
amberjackman <feverhong@vivaldi.net>
2022-09-27 23:03:54 +0900

    login page

--- js/common.js
+++ js/common.js
@@ -18,5 +18,5 @@ searchInputEl.addEventListener('blur', function () {
 
 
 
-const thisYear = document.querySelector('this-year');
+const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear(); // 2022
